770ba761489ed0bcc3ef0c4f8616f44590f91e8c
shuvo87sa <sabbirnew2023@gmail.com>
2024-11-29 12:04:03 +0600

    slider work properly

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -4,17 +4,34 @@ import './slider.scss'
 function Slider({ images }){
     const [imageIndex, setImageIndex] = useState(null);
 
+    const changeSlide = (direction)=> {
+        if(direction == "left"){
+            if(imageIndex === 0){
+                setImageIndex(images.length -1)
+            }else{
+                setImageIndex(imageIndex -1)
+            }
+        }
+        else{
+            if(imageIndex === images.length -1){
+                setImageIndex(0);
+            }else{
+                setImageIndex(imageIndex + 1)
+            }
+        }
+    };
+
     return (
         <div className='slider'> 
            {imageIndex !== null && (
             <div className="fullSlider">
-                <div className="arrow">
+                <div className="arrow" onClick={()=>changeSlide("left")}>
                     <img src="/arrow.png" alt="" />
                 </div>
                 <div className="imgContainer">
                     <img src={images[imageIndex]} alt="" />
                 </div>
-                <div className="arrow">
+                <div className="arrow" onClick={()=>changeSlide("right")}>
                     <img src="/arrow.png" className='right' alt="" />
                 </div>
                 <div className="close" onClick={()=>setImageIndex(null)}>X</div>
